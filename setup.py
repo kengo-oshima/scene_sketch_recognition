1d3ef354671fe22d14333d76a248475c81e17f43
unknown <kengocardistry@gmail.com>
2020-05-21 04:14:56 +0900

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Study of Improvement of Scene Sketch Image Recognition',
+    author='Kengo Oshima',
+    license='MIT',
+)
