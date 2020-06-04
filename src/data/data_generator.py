caf36ca27eece0f49cd65f62c18ac5c491fbb496
kengo-oshima <kengocardistry@gmail.com>
2020-06-04 16:48:46 +0900

    create new file

---
+++ src/data/data_generator.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+
+
+# ---------------------
+# データ読み込み・拡張
+# ---------------------
+class GenData():
+    def __init__(self):
+        pass
