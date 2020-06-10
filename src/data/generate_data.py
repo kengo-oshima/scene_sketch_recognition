358fd0f49cf887c8c0612afa293d93b7a9ba1137
kengo-oshima <kengocardistry@gmail.com>
2020-06-10 15:43:37 +0900

    途中経過とcreate new file

--- src/data/generate_data.py
+++ src/data/generate_data.py
@@ -3,6 +3,7 @@ import os
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # ---------------------
 # データ読み込み・拡張
@@ -11,6 +12,15 @@ class GenData():
     def __init__(self, pd_obj):
         self.pd_obj = pd_obj
 
+    def load_npdata(self, data_dir_path):
+        file_list = os.listdir(data_dir_path)
+        data_list = []
+        for category_index, file in enumerate(file_list):
+            array = np.load(os.path.join(data_dir_path, file))
+            for data in array:
+                data_list.append(data)
+
+        return data_list
 
 
 
@@ -23,5 +33,7 @@ if __name__ == '__main__':
     csv_file_path = os.path.join(root_dir_path, r"data\label\scene_sketch_label.csv")
 
     df = pd.read_csv(csv_file_path)
+    data = GenData(df)
+    img_data = data.load_npdata(img_dir_path)
 
     print(df)
